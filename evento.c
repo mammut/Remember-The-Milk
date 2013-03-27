146a33c8a8b3cc5242d5389113786dcfab66a275
mammut <irken.cs@gmail.com>
2013-03-26 21:59:32 -0400

    Implementada la primera version de listar_eventos. a probar

--- evento.c
+++ evento.c
@@ -40,7 +40,20 @@ void concretar_evento(evento *e){
  *
  */
 void listar_eventos(){
+	FILE *db;
+	evento reader;
 
+	if ( (db = fopen("db.dat", "rb")) == NULL) {
+		fprintf(stderr, "Error. No se pudo abrir el archivo 'db.dat'\n");
+		return;
+	}
+
+	printf("ID | Titulo | Estado\n");
+	while ( fread(&reader, sizeof(evento), 1, db) == 1) {
+		printf("%d | %s | %s\n", reader.id, reader.titl, reader.estado == 1 ? "Realizado" : "No realiado");
+	}
+
+	fclose(db);
 }
 
 /**
