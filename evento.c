89b67adc78562fe3d9099f39e9c3a47400f43cd7
mammut <irken.cs@gmail.com>
2013-03-27 11:22:52 -0400

    Primera implementacion de mostrar_evento

--- evento.c
+++ evento.c
@@ -12,7 +12,27 @@ void nuevo_evento(evento *e){
  *
  */
 void mostrar_evento(evento *e){
+	FILE *db;
+	evento reader;
 
+	if ( (fopen("db.dat", "rb")) == NULL) {
+		fprintf(stderr, "Error. No se pudo abrir el archivo 'db.dat'\n");
+		return;
+	}
+
+	while (fread(&reader, sizeof(evento), 1, db) == 1) {
+		if (reader.id == e->id) {
+			printf("Evento #%d :: %s\n", reader.id,reader.titl);
+			printf("Este es un evento\n");
+			printf("%s\n", reader.estado == 0 ? "No Realizado" : "Realizado");
+			fclose(db);
+			return;
+		}
+	}
+
+	fclose(db);
+
+	printf("El evento #%d no existe\n", e->id);
 }
 
 /**
