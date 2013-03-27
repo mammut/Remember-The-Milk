da52808ebd75e7fe2b3474d4befe402ce671c656
mammut <irken.cs@gmail.com>
2013-03-27 15:53:17 -0400

    Primera implementacion de modificar_evento

--- evento.c
+++ evento.c
@@ -23,10 +23,44 @@ void eliminar_evento(evento *e){
 }
 
 /**
- *
+ * Modifica informacion de un evento determinado en el registro de eventos.
+ * PARAMS.: - e: puntero a evento inicializado (memoria previamente
+ *           asignada).
  */
 void modificar_evento(evento *e){
+	FILE *db;
+	evento reader;
 
+	if ( (db = fopen("db.dat", "rb+")) == NULL) {
+		fprintf(stderr, "Error. No se pudo abrir el archivo 'db.dat'\n");
+		return;
+	}
+
+	while (fread(&reader, sizeof(evento), 1, db) == 1) {
+		if (reader.id == e->id) {
+			switch(e->estado) {
+				case 2:
+					sprintf(reader.titl, "%s", e->titl);
+					break;
+				case 3:
+					sprintf(reader.desc, "%s", e->desc);
+					break;
+				case 4:
+					sprintf(reader.titl, "%s", e->titl);
+					sprintf(reader.desc, "%s", e->desc);
+					break;
+			}
+			fseek(db, -sizeof(evento), SEEK_CUR);
+			fwrite(&reader, sizeof(evento), 1, db);
+			fclose(db);
+			printf("Evento #%d modificado exitosamente!", e->id);
+			return;
+		}
+	}
+
+	fclose(db);
+
+	printf("El evento #%d no existe\n", e->id);
 }
 
 /**
