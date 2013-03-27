c4ef47523336e33328e7df241c07f974340a98fd
mammut <irken.cs@gmail.com>
2013-03-27 17:41:46 -0400

    Agregado mensaje de "no hay eventos en el registro!" cuando el archivo esta vacio

--- evento.c
+++ evento.c
@@ -42,16 +42,21 @@ void concretar_evento(evento *e){
 void listar_eventos(){
 	FILE *db;
 	evento reader;
+	int printed = 0;
 
 	if ( (db = fopen("db.dat", "rb")) == NULL) {
 		fprintf(stderr, "Error. No se pudo abrir el archivo 'db.dat'\n");
 		return;
 	}
 
-	printf("ID | Titulo | Estado\n");
+
 	while ( fread(&reader, sizeof(evento), 1, db) == 1) {
+		if (printed++ == 0)	printf("ID | Titulo | Estado\n");
+
 		printf("%d | %s | %s\n", reader.id, reader.titl, reader.estado == 1 ? "Realizado" : "No realiado");
 	}
+	if (printed == 0)
+		printf("No hay eventos en el registro!\n");
 
 	fclose(db);
 }
